0298815c9e20ec6c4f35dbeaa525a014a780e42b
Richard Alvarez <alvarez.richard@gmail.com>
2015-09-16 20:53:27 -0400

    working nodemon setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 var gulp = require('gulp'),
-	babel = require("gulp-babel");
+	babel = require('gulp-babel'),
+	nodemon = require("gulp-nodemon");
 
-gulp.task("default", function () {
+gulp.task('build', function () {
   return gulp.src("src/app.js")
     .pipe(babel({stage : 0}))
     .pipe(gulp.dest("dist"));
 });
+
+gulp.task('start', function () {
+  nodemon({
+    script: 'dist/app.js',
+    tasks: ['build'],
+    ignore: ['dist/*'],
+    ext: 'js html',
+    env: { 'NODE_ENV': 'development' }
+  })
+})
+
+gulp.task('default', ['build', 'start']);
