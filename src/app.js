67aea2a12b8cdb84d44e62e9cf986c049320ad00
Richard Alvarez <alvarez.richard@gmail.com>
2015-09-17 10:02:38 -0400

    initial run of scraping first few pages of OT

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
-var x = [for (i of [ 1, 2, 3 ]) i*i ]; 
-// [ 1, 4, 9 ]
-console.log(x);
+let Xray = require('x-ray'),
+	xray = Xray(),
+	threadUrl = 'http://www.neogaf.com/forum/showthread.php?t=1109852';
+
+
+xray(threadUrl, 'div[id^=edit]', [{
+	postNumber: '.post-meta .right strong',
+	poster: '.postbit-details-username a',
+	subject: '.post-meta-border strong', 
+	body: '.post'
+}])
+.paginate('a[rel="next"]@href')
+.delay(50, 100)
+.limit(3)
+.write('out.json')
