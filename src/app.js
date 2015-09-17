7f1cb759126c0b8c8589b296e2c013effc53bb49
Richard Alvarez <alvarez.richard@gmail.com>
2015-09-17 10:20:34 -0400

    extracted all posts

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@ let Xray = require('x-ray'),
 	threadUrl = 'http://www.neogaf.com/forum/showthread.php?t=1109852';
 
 
-xray(threadUrl, 'div[id^=edit]', [{
-	postNumber: '.post-meta .right strong',
-	poster: '.postbit-details-username a',
-	subject: '.post-meta-border strong', 
-	body: '.post'
-}])
-.paginate('a[rel="next"]@href')
-.delay(50, 100)
-.limit(3)
-.write('out.json')
+function extractAllPosts() {
+		xray(threadUrl, 'div[id^=edit]', [{
+		postNumber: '.post-meta .right strong',
+		poster: '.postbit-details-username a',
+		subject: '.post-meta-border strong', 
+		body: '.post'
+	}])
+	.paginate('a[rel="next"]@href')
+	.write('posts.json')
+}
