244374a1e9f08bcffff37c38098fea84fa948fd1
Richard Alvarez <alvarez.richard@gmail.com>
2015-09-17 15:53:33 -0400

    prep db files

--- src/app.js
+++ src/app.js
@@ -1,15 +1,3 @@
 let Xray = require('x-ray'),
-	xray = Xray(),
-	threadUrl = 'http://www.neogaf.com/forum/showthread.php?t=1109852';
+	xray = Xray();
 
-
-function extractAllPosts() {
-		xray(threadUrl, 'div[id^=edit]', [{
-		postNumber: '.post-meta .right strong',
-		poster: '.postbit-details-username a',
-		subject: '.post-meta-border strong', 
-		body: '.post'
-	}])
-	.paginate('a[rel="next"]@href')
-	.write('posts.json')
-}
